d9918de11e6b4bf3ab68124d86d692753cacd710
Polina <75391420+wezbicka@users.noreply.github.com>
2022-08-27 10:42:48 +0300

    Download first comics

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ def download_image(image_url, download_path):
     response.raise_for_status()
     with open(download_path, 'wb') as file:
         file.write(response.content)
+
+
+def main():
+    url = "https://xkcd.com/353"
+    response = requests.get(f"{url}/info.0.json")
+    response.raise_for_status()
+    image_url = response.json()["img"]
+    download_image(image_url, "картинка.png")
+
+
+if __name__ == "__main__":
+    main()
