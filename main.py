aa2ef364338be49275714153d6ac924947fdd3bb
Polina <75391420+wezbicka@users.noreply.github.com>
2023-04-09 00:00:09 +0300

    Gotten photo url to upload

--- main.py
+++ main.py
@@ -16,6 +16,19 @@ def get_groups(token):
     print(response.json())
 
 
+def get_photo_url(token, group_id):
+    """Returns the server address for uploading a photo to a user's or community's wall."""
+    url = "https://api.vk.com/method/photos.getWallUploadServer"
+    params = {
+        'access_token': token,
+        "group_id": group_id,
+        'v': 5.131,
+    }
+    response = requests.post(url, params=params)
+    response.raise_for_status()
+    print(response.content)
+
+
 def download_image(image_url, download_path):
     response = requests.get(image_url)
     response.raise_for_status()
@@ -27,13 +40,15 @@ def main():
     load_dotenv()
     client_id = os.environ['CLIENT_ID']
     vk_token = os.environ['ACCESS_TOKEN']
-    url = "https://xkcd.com/353"
-    response = requests.get(f"{url}/info.0.json")
-    response.raise_for_status()
-    image_url = response.json()["img"]
-    comment = response.json()["alt"]
-    print(comment)
-    download_image(image_url, "картинка.png")
+    group_id = 215590113
+    # url = "https://xkcd.com/353"
+    # response = requests.get(f"{url}/info.0.json")
+    # response.raise_for_status()
+    # image_url = response.json()["img"]
+    # comment = response.json()["alt"]
+    # print(comment)
+    # download_image(image_url, "картинка.png")
+    get_photo_url(vk_token, group_id)
     get_groups(vk_token)
 
 
