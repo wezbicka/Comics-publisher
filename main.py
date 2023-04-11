a6263ac0ead3373829bb70439e2534cf27dbb5a0
Polina <75391420+wezbicka@users.noreply.github.com>
2023-04-11 12:36:28 +0300

    Deleted temporary files in case of an error

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 
 logger = logging.getLogger()
-logging.basicConfig(filename="py_log.log",filemode="w")
+logging.basicConfig(filename="py_log.log", filemode="w")
 
 
 class VkApiError(Exception):
@@ -93,7 +93,7 @@ def exist_errors(json_response):
     if 'error' in json_response.keys():
         logging.error(json_response['error'])
         raise VkApiError(json_response['error']['error_msg'])
-    
+
 
 def download_image(image_url, download_path):
     response = requests.get(image_url)
@@ -111,24 +111,25 @@ def main():
     comics_amount = get_last_comic_index()
     comic_number = random.randint(0, comics_amount)
     image_url, comment = get_comic(comic_number)
-    download_image(image_url, file_path)
-    upload_url = get_upload_url(vk_token, group_id, version)
-    server_response = upload_photo_to_server(upload_url, file_path)
-    media_id, owner_id = save_photo_to_album(
-        vk_token,
-        group_id,
-        server_response,
-        version,
-    )
-    post_photo_to_wall(
-        vk_token,
-        group_id,
-        media_id,
-        owner_id,
-        comment,
-        version,
-    )
-    if os.path.isfile(file_path):
+    try:
+        download_image(image_url, file_path)
+        upload_url = get_upload_url(vk_token, group_id, version)
+        server_response = upload_photo_to_server(upload_url, file_path)
+        media_id, owner_id = save_photo_to_album(
+            vk_token,
+            group_id,
+            server_response,
+            version,
+        )
+        post_photo_to_wall(
+            vk_token,
+            group_id,
+            media_id,
+            owner_id,
+            comment,
+            version,
+        )
+    finally:
         os.remove(file_path)
 
 
